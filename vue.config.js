11948a6c7c089043178a676451b7237cd1c255d4
QingJ <108940482+QingJ01@users.noreply.github.com>
2025-10-07 12:55:44 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -38,7 +38,7 @@ module.exports = {
       entry: 'src/main.js',
       template: 'public/index.html',
       filename: 'index.html',
-      title: 'YesPlayMusic',
+      title: 'QingJ Music',
       chunks: ['main', 'chunk-vendors', 'chunk-common', 'index'],
     },
   },
